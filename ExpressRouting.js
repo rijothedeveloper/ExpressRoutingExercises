d54ebeb0b619cbc0182bdec28bb19becc8e2c369
Rijo George <rijogeorge@rijos-mbp-2.lan>
2022-01-23 13:21:52 -0800

    mean done

--- ExpressRouting.js
+++ ExpressRouting.js
@@ -1,7 +1,32 @@
-const express = require('express');
+const { response } = require("express");
+const express = require("express");
 
 const app = express();
 
+app.get("/mean", (req, res) => {
+  const numStr = req.query.nums;
+  const nums = numStr.split(",");
+  const mean = calculateMean(nums);
+  const response = createResponse("mean", mean);
+  return res.json(response);
+});
+
+function calculateMean(nums) {
+  let total = 0;
+  for (let num of nums) {
+    no = parseInt(num);
+    total += no;
+  }
+  return total / nums.length;
+}
+
+function createResponse(operation, value) {
+  const response = {};
+  response.operation = operation;
+  response.value = value;
+  return response;
+}
+
 app.listen(3000, function () {
-  console.log('App on port 3000');
-})
+  console.log("App on port 3000");
+});
